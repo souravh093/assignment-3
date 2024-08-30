9e69aea376c48fbbb42d8e58fb8840d0ae8ba57e
Sourave halder <souravh093@gmail.com>
2024-08-30 19:33:14 +0600

    final deployment

--- dist/app.js
+++ dist/app.js
@@ -13,7 +13,7 @@ const app = (0, express_1.default)();
 // parsers
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
-app.use((0, cors_1.default)({ origin: ['https://bike-rental-client-zeta.vercel.app'], credentials: true }));
+app.use((0, cors_1.default)({ credentials: true }));
 // app routes
 app.use('/api', routes_1.default);
 // global error handle zod, mongoose, custom error, error, cast error etc..
